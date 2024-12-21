92187a21713b9438f0071eb7eba6eaac3d368e89
NextOps <contact@nextops.in>
2024-12-21 17:06:58 +0700

    Code updated

--- Terraform/01_providers/provider.tf
+++ Terraform/01_providers/provider.tf
@@ -14,10 +14,10 @@ provider "azurerm" {
   resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 
-  # client_id       = "92d947f1-089b-4deb-b0b7-d4dee0612cc6"
-  # client_secret   = "ywj8Q~L8L~uULgqlnjelT9za5GdDY_UFpi0uJc-I"
-  # tenant_id       = "9085ff8c-8807-4ff8-a403-ea470525cda6"
-  # subscription_id = "5c5f2b33-138d-47aa-b0e4-9b9fd849425a"
+  # client_id       = "xxxxxx-xxxxxx-xxxxxx-xxx-xxx"
+  # client_secret   = "xxxxxx-xxxxxx-xxxxxx-xxx-xxx""
+  # tenant_id       = "xxxxxx-xxxxxx-xxxxxx-xxx-xxx"
+  # subscription_id = "xxxxxx-xxxxxx-xxxxxx-xxx-xxx""
  
 }
 
